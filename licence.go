f737ac9f45b3de62df1ec5395f545d501707a007
mehmetalisavas <mehmetali.savas@yandex.com>
2019-01-20 01:48:07 +0100

    licence: added licende related informations

---
+++ licence.go
@@ -0,0 +1,35 @@
+package cacoo
+
+import (
+	"context"
+	"fmt"
+)
+
+// LicenseService handles licence related methods
+type LicenseService service
+
+// License represents the information about account licence
+type License struct {
+	CanCreateSharedFolder             *bool   `json:"canCreateSharedFolder,omitempty"`
+	CanCreateSheet                    *bool   `json:"canCreateSheet,omitempty"`
+	CanExportVectorFormat             *bool   `json:"canExportVectorFormat,omitempty"`
+	MaxNumberOfSharersPerDiagram      *int    `json:"maxNumberOfSharersPerDiagram,omitempty"`
+	MaxNumberOfSharersPerSharedFolder *int    `json:"maxNumberOfSharersPerSharedFolder,omitempty"`
+	Plan                              *string `json:"plan,omitempty"`
+	RemainingSharedFolders            *int    `json:"remainingSharedFolders,omitempty"`
+	RemainingSheets                   *int    `json:"remainingSheets,omitempty"`
+}
+
+// GetLicense gives information about the account licence
+func (ls *LicenseService) GetLicense(ctx context.Context) (*License, *Response, error) {
+	u := fmt.Sprintf("%s", "account/license.json")
+
+	l := new(License)
+
+	resp, err := ls.client.Get(ctx, u, &l)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return l, resp, nil
+}
