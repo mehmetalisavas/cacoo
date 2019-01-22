0b0b057232036c632488fa5db8f615c0892fc67e
mehmetalisavas <mehmetali.savas@yandex.com>
2019-01-22 01:15:53 +0100

    test: add test cases for diagram methods

--- diagram_test.go
+++ diagram_test.go
@@ -7,15 +7,13 @@ import (
 	"reflect"
 	"testing"
 	"time"
-
-	"github.com/bookerzzz/grok"
 )
 
-func TestListDiagrams(t *testing.T) {
+func TestDiagramListDiagrams(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/users/1MUJPfNEEeVUox15.json", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/diagrams.json", func(w http.ResponseWriter, r *http.Request) {
 		method(t, r, "GET")
 		fmt.Fprint(w, `{
 		    "result": [
@@ -54,7 +52,279 @@ func TestListDiagrams(t *testing.T) {
 	if err != nil {
 		t.Errorf("diagrams.listdiagrams returned error: %v", err)
 	}
-	dr := DiagramResult{
+
+	want := testDiagrams(t)
+
+	if !reflect.DeepEqual(diagrams, want) {
+		t.Errorf("want: %v, but got: %v", want, diagrams)
+	}
+}
+
+func TestDiagramGetDiagram(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/diagrams/00e77f4dc9973517.json", func(w http.ResponseWriter, r *http.Request) {
+		method(t, r, "GET")
+		fmt.Fprint(w, `{
+    "url": "https://cacoo.com/diagrams/00e77f4dc9973517",
+    "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517.png",
+    "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png",
+    "diagramId": "00e77f4dc9973517",
+    "title": "Wireframe",
+    "description": "Current project wireframe",
+    "security": "url",
+    "type": "normal",
+    "ownerName": "1MUJPfNEEeVUox15",
+    "ownerNickname": "Yoko",
+    "owner": {
+        "name": "1MUJPfNEEeVUox15",
+        "nickname": "Yoko",
+        "type": "cacoo",
+        "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+    },
+    "editing": true,
+    "own": true,
+    "shared": false,
+    "folderId": 10001,
+    "folderName": "Design",
+    "sheetCount": 3,
+    "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "updated": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "sheets": [
+        {
+            "url": "https://cacoo.com/diagrams/00e77f4dc9973517#aaaa",
+            "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517-aaaa.png",
+            "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517-aaaa.png",
+            "uid": "aaaa",
+            "name": "sheet1",
+            "width": 200,
+            "height": 100
+        }
+    ],
+    "comments": [
+        {
+            "user": {
+                "name": "1MUJPfNEEeVUox15",
+                "nickname": "Yoko",
+                "type": "cacoo",
+                "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+            },
+            "content": "comment 1",
+            "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+            "updated": "Mon, 10 Aug 2009 17:00:00 +0900"
+        }
+    ]
+}`)
+	})
+
+	diagram, _, err := client.Diagram.GetDiagram(context.Background(), "00e77f4dc9973517")
+	if err != nil {
+		t.Errorf("diagram.getdiagram returns error: %v", err)
+	}
+
+	want := testDiagramResultWithSheetAndComment(t)
+
+	if !reflect.DeepEqual(diagram, want) {
+		t.Errorf("want: %v, but got: %v", want, diagram)
+	}
+}
+
+func TestDiagramCreate(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/diagrams/create.json", func(w http.ResponseWriter, r *http.Request) {
+		method(t, r, "POST")
+		fmt.Fprint(w, `{
+    "url": "https://cacoo.com/diagrams/00e77f4dc9973517",
+    "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517.png",
+    "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png",
+    "diagramId": "00e77f4dc9973517",
+    "title": "Wireframe",
+    "description": "Current project wireframe",
+    "security": "url",
+    "type": "normal",
+    "ownerName": "1MUJPfNEEeVUox15",
+    "ownerNickname": "Yoko",
+    "owner": {
+        "name": "1MUJPfNEEeVUox15",
+        "nickname": "Yoko",
+        "type": "cacoo",
+        "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+    },
+    "editing": true,
+    "own": true,
+    "shared": false,
+    "folderId": 10001,
+    "folderName": "Design",
+    "sheetCount": 3,
+    "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "updated": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "sheets": [
+        {
+            "url": "https://cacoo.com/diagrams/00e77f4dc9973517#aaaa",
+            "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517-aaaa.png",
+            "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517-aaaa.png",
+            "uid": "aaaa",
+            "name": "sheet1",
+            "width": 200,
+            "height": 100
+        }
+    ],
+    "comments": [
+        {
+            "user": {
+                "name": "1MUJPfNEEeVUox15",
+                "nickname": "Yoko",
+                "type": "cacoo",
+                "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+            },
+            "content": "comment 1",
+            "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+            "updated": "Mon, 10 Aug 2009 17:00:00 +0900"
+        }
+    ]
+}`)
+	})
+
+	generatedDiagram := testGenerateDiagramOption(t)
+	diagram, _, err := client.Diagram.Create(context.Background(), generatedDiagram)
+	if err != nil {
+		t.Errorf("diagram.create returns err: %q", err)
+	}
+
+	want := testDiagramResultWithSheetAndComment(t)
+
+	if !reflect.DeepEqual(diagram, want) {
+		t.Errorf("want: %v, but got: %v", want, diagram)
+	}
+}
+
+func TestDiagramGet(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/diagrams/create.json", func(w http.ResponseWriter, r *http.Request) {
+		method(t, r, "GET")
+		fmt.Fprint(w, `{
+    "url": "https://cacoo.com/diagrams/00e77f4dc9973517",
+    "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517.png",
+    "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png",
+    "diagramId": "00e77f4dc9973517",
+    "title": "Wireframe",
+    "description": "Current project wireframe",
+    "security": "url",
+    "type": "normal",
+    "ownerName": "1MUJPfNEEeVUox15",
+    "ownerNickname": "Yoko",
+    "owner": {
+        "name": "1MUJPfNEEeVUox15",
+        "nickname": "Yoko",
+        "type": "cacoo",
+        "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+    },
+    "editing": true,
+    "own": true,
+    "shared": false,
+    "folderId": 10001,
+    "folderName": "Design",
+    "sheetCount": 3,
+    "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "updated": "Mon, 10 Aug 2009 17:00:00 +0900",
+    "sheets": [
+        {
+            "url": "https://cacoo.com/diagrams/00e77f4dc9973517#aaaa",
+            "imageUrl": "https://cacoo.com/diagrams/00e77f4dc9973517-aaaa.png",
+            "imageUrlForApi": "https://cacoo.com/api/v1/diagrams/00e77f4dc9973517-aaaa.png",
+            "uid": "aaaa",
+            "name": "sheet1",
+            "width": 200,
+            "height": 100
+        }
+    ],
+    "comments": [
+        {
+            "user": {
+                "name": "1MUJPfNEEeVUox15",
+                "nickname": "Yoko",
+                "type": "cacoo",
+                "imageUrl": "https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"
+            },
+            "content": "comment 1",
+            "created": "Mon, 10 Aug 2009 17:00:00 +0900",
+            "updated": "Mon, 10 Aug 2009 17:00:00 +0900"
+        }
+    ]
+}`)
+	})
+
+	diagram, _, err := client.Diagram.Get(context.Background())
+	if err != nil {
+		t.Errorf("diagram.create returns err: %q", err)
+	}
+
+	want := testDiagramResultWithSheetAndComment(t)
+
+	if !reflect.DeepEqual(diagram, want) {
+		t.Errorf("want: %v, but got: %v", want, diagram)
+	}
+}
+
+func TestDiagramDelete(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/diagrams/00e77f4dc9973517/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		method(t, r, "GET")
+		fmt.Fprint(w, `{}`)
+	})
+
+	resp, err := client.Diagram.Delete(context.Background(), "00e77f4dc9973517")
+	if err != nil {
+		t.Errorf("diagram.create returns err: %q", err)
+	}
+	if resp == nil {
+		t.Error("response should be nil, but got nil")
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("status code should equal to %d, but got: %d", http.StatusOK, resp.StatusCode)
+	}
+}
+
+func TestDiagramGetToken(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/diagrams/00e77f4dc9973517/editor/token.json", func(w http.ResponseWriter, r *http.Request) {
+		method(t, r, "GET")
+		fmt.Fprint(w, `{
+		    "token": "nITfHr0rxfeAxdCn"
+		}`)
+	})
+
+	token, _, err := client.Diagram.GetToken(context.Background(), "00e77f4dc9973517")
+	if err != nil {
+		t.Errorf("token.gettoken returns err: %q", err)
+	}
+
+	want := testToken(t)
+
+	if !reflect.DeepEqual(token, want) {
+		t.Errorf("want: %v, but got: %v", want, token)
+	}
+}
+
+func testAccount() *Account {
+	return &Account{
+		Name:     String("1MUJPfNEEeVUox15"),
+		Nickname: String("Yoko"),
+		Type:     String("cacoo"),
+		ImageURL: String("https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"),
+	}
+}
+func testDiagramResult(t *testing.T) DiagramResult {
+	return DiagramResult{
 		URL:            String("https://cacoo.com/diagrams/00e77f4dc9973517"),
 		ImageURL:       String("https://cacoo.com/diagrams/00e77f4dc9973517.png"),
 		ImageURLForAPI: String("https://cacoo.com/api/v1/diagrams/00e77f4dc9973517.png"),
@@ -65,100 +335,75 @@ func TestListDiagrams(t *testing.T) {
 		Type:           String("normal"),
 		OwnerName:      String("1MUJPfNEEeVUox15"),
 		OwnerNickname:  String("Yoko"),
-		Owner: &Account{
-			Name:     String("1MUJPfNEEeVUox15"),
-			Nickname: String("Yoko"),
-			Type:     String("cacoo"),
-			ImageURL: String("https://cacoo.com/account/1MUJPfNEEeVUox15/image/32x32"),
-		},
-		Editing:    Bool(true),
-		Own:        Bool(true),
-		Shared:     Bool(false),
-		FolderID:   Int(10001),
-		FolderName: String("Design"),
-		SheetCount: Int(3),
-		Created:    &CacooTime{time.Date(dateFormat)},
-		Updated:    String("Mon, 10 Aug 2009 17:00:00 +0900"),
+		Owner:          testAccount(),
+		Editing:        Bool(true),
+		Own:            Bool(true),
+		Shared:         Bool(false),
+		FolderID:       Int(10001),
+		FolderName:     String("Design"),
+		SheetCount:     Int(3),
+		Created:        testTime(t),
+		Updated:        testTime(t),
 	}
-	want := &Diagrams{
-		Result: []DiagramResult{dr},
+}
+func testSheet() Sheet {
+	return Sheet{
+		Height:         Int(100),
+		ImageURL:       String("https://cacoo.com/diagrams/00e77f4dc9973517-aaaa.png"),
+		ImageURLForAPI: String("https://cacoo.com/api/v1/diagrams/00e77f4dc9973517-aaaa.png"),
+		Name:           String("sheet1"),
+		UID:            String("aaaa"),
+		URL:            String("https://cacoo.com/diagrams/00e77f4dc9973517#aaaa"),
+		Width:          Int(200),
+	}
+}
+func testDiagramResultWithSheetAndComment(t *testing.T) *DiagramResult {
+	comment := testComment(t)
+
+	tdr := testDiagramResult(t)
+	dr := &DiagramResult{}
+	dr = &tdr
+	dr.Comments = append(dr.Comments, comment)
+	dr.Sheets = append(dr.Sheets, testSheet())
+
+	return dr
+}
+
+func testComment(t *testing.T) Comment {
+	return Comment{
+		User:    testAccount(),
+		Content: String("comment 1"),
+		Updated: testTime(t),
+		Created: testTime(t),
+	}
+}
+
+func testTime(t *testing.T) *CacooTime {
+	testTime, err := time.Parse(dateFormat, "Mon, 10 Aug 2009 17:00:00 +0900")
+	if err != nil {
+		t.Errorf("error should be nil but got: %q", err)
+	}
+
+	return &CacooTime{testTime}
+}
+
+func testDiagrams(t *testing.T) *Diagrams {
+	return &Diagrams{
+		Result: []DiagramResult{testDiagramResult(t)},
 		Count:  Int(1),
 	}
-
-	if !reflect.DeepEqual(user, want) {
-		t.Errorf("want: %v, but got: %v", want, user)
+}
+func testToken(t *testing.T) *Editor {
+	return &Editor{
+		Token: String("nITfHr0rxfeAxdCn"),
 	}
 }
 
-func TestDiagramWithName(t *testing.T) {
-	c := NewClient(TestToken)
-	// c := NewClient("asd")
-	name := "rkzgn5jGe2asBRCQ"
-	diagram, _, err := c.Diagram.GetDiagram(context.Background(), name)
-	if err != nil {
-		fmt.Println("err is:", err)
-	}
-
-	grok.V(diagram)
-	fmt.Println(diagram)
-	_ = diagram
-}
-func TestDiagramGet(t *testing.T) {
-	c := NewClient(TestToken)
-	// c := NewClient("asd")
-	name := "rkzgn5jGe2asBRCQ"
-	diagram, _, err := c.Diagram.GetDiagram(context.Background(), name)
-	if err != nil {
-		fmt.Println("err is:", err)
-	}
-
-	grok.V(diagram)
-	fmt.Println(diagram)
-	_ = diagram
-}
-func TestDiagramCreate(t *testing.T) {
-	c := NewClient(TestToken)
-	// c := NewClient("asd")
-	opt := &DiagramOption{
-		FolderID:    351198,
-		Title:       "diagram-title test",
-		Description: "diagram-description test2",
+func testGenerateDiagramOption(t *testing.T) *DiagramOption {
+	return &DiagramOption{
+		FolderID:    10001,
+		Title:       "Wireframe",
+		Description: "Current project wireframe",
 		Security:    "url",
 	}
-
-	diagram, _, err := c.Diagram.Create(context.Background(), opt)
-	if err != nil {
-		fmt.Println("err is:", err)
-	}
-
-	grok.V(diagram)
-	fmt.Println(diagram)
-	_ = diagram
-}
-func TestDiagramDelete(t *testing.T) {
-	c := NewClient(TestToken)
-	// c := NewClient("asd")
-
-	diagramID := "voD5W1IRnsizEpcm"
-
-	_, err := c.Diagram.Delete(context.Background(), diagramID)
-	if err != nil {
-		fmt.Println("err is:", err)
-	}
-
-}
-
-func TestDiagramToken(t *testing.T) {
-	c := NewClient(TestToken)
-	// c := NewClient("asd")
-
-	diagramID := "rkzgn5jGe2asBRCQ"
-
-	token, _, err := c.Diagram.GetToken(context.Background(), diagramID)
-	if err != nil {
-		fmt.Println("err is:", err)
-	}
-
-	_ = token
-
 }
